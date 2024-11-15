58cda7cdfb017f5f79e96df23053b6dbcb19c1a7
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-11-15 16:32:23 +0530

    Initial Commit

---
+++ SwiftUI-Animations/SwiftUI_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationsApp.swift
+//  SwiftUI-Animations
+//
+//  Created by Selvarajan on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
